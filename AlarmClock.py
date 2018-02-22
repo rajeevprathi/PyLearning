cebc7b9968941d427a8119b48728fb1401880dbe
Rajeev Prathi <rajeevprathi@Rajeevs-MacBook-Pro.local>
2018-02-22 14:03:20 -0800

    minor changes to alarm clock

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,22 +1,31 @@
+import time
 import webbrowser
-import datetime
 
-hour = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)
-minute = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59)
+hour = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23')
+minute = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59')
 
 try:
-    user_time = input("Please enter the time for alarm (format HHMM): ")
-    modified_user_time = datetime.datetime.strptime(user_time,"%H%M")
-    print(time.strftime(modified_user_time))
+    user_hour = input("Please enter the hour for alarm (24hour, format HH): ")
+    if str(user_hour) in hour:
+        pass
+    user_minute = input("Please enter minutes for alarm (format MM): ")
+    if str(user_minute) in minute:
+        pass
+    user_time = user_hour + ":" + user_minute
+    print("Your alarm is set for " + user_time +"hrs")
+
 except:
-    print("Please enter the time in correct format")
+    print("Please enter the time in correct format - ERROR_CODE-001")
+
+try:
+    while str(user_time) != str(time.strftime("%H:%M")):
+        print("Waiting for the alarm ... ")
+        time.sleep(30)
+
+    def youtube_links():
+        webbrowser.open('https://www.youtube.com/watch?v=aBt8fN7mJNg', new=2)
+
+except:
+    print("Oops.. something went wrong - ERROR_CODE-002")
 
 
-
-"""
-while str(user_time) != str(time.strftime("%H:%M")):
-    print("Waiting for time to match")
-    time.sleep(30)
-    
-webbrowser.open('https://www.youtube.com/watch?v=aBt8fN7mJNg', new=2)
-    """
